6f336b61aab7eee1256f38e9922381b0b62693ba
jfa <jfa@opencascade.com>
2022-05-11 13:57:50 +0300

    [bos #29476] [EDF] (2022-T1) SHAPER: Offset in sketch

--- src/GeomAlgoAPI/GeomAlgoAPI_Offset.cpp
+++ src/GeomAlgoAPI/GeomAlgoAPI_Offset.cpp
@@ -64,7 +64,8 @@ void GeomAlgoAPI_Offset::generated(const GeomShapePtr theOldShape,
 
 GeomAlgoAPI_Offset::GeomAlgoAPI_Offset(const GeomPlanePtr& thePlane,
                                        const GeomShapePtr& theEdgeOrWire,
-                                       const double theOffsetValue)
+                                       const double theOffsetValue,
+                                       const GeomAlgoAPI_OffsetJoint theJoint)
 {
   // 1. Make wire from edge, if need
   TopoDS_Wire aWire;
@@ -92,11 +93,23 @@ GeomAlgoAPI_Offset::GeomAlgoAPI_Offset(const GeomPlanePtr& thePlane,
   setImpl(aParal);
   setBuilderType(OCCT_BRepBuilderAPI_MakeShape);
 
+  // Joint type
+  GeomAbs_JoinType aJoin = GeomAbs_Arc; // default mode, corresponding to KeepDistance
+  if (theJoint == GeomAlgoAPI_OffsetJoint::Lines)
+    aJoin = GeomAbs_Intersection;
+  // for GeomAlgoAPI_OffsetJoint::Arcs do the same as for KeepDistance
+
   Standard_Boolean isOpenResult = !aWire.Closed();
-  aParal->Init(aFace, GeomAbs_Arc, isOpenResult);
+  aParal->Init(aFace, aJoin, isOpenResult);
   aParal->Perform(theOffsetValue, 0.);
   if (aParal->IsDone()) {
     TopoDS_Shape anOffset = aParal->Shape();
+
+    if (theJoint == GeomAlgoAPI_OffsetJoint::Arcs) {
+      // TODO: process Arcs case - make fillets on straight
+      //       edges intersections, except too short edges
+    }
+
     GeomShapePtr aResult(new GeomAPI_Shape());
     aResult->setImpl(new TopoDS_Shape(anOffset));
     setShape(aResult);
